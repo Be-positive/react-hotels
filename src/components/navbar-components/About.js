c5351a92e78920b3e834abc495c3a299aaaff640
Vladimir <felps0308@gmail.com>
2022-11-18 18:42:15 +0300

    changes in Navbar and icon added

--- src/components/navbar-components/About.js
+++ src/components/navbar-components/About.js
@@ -6,8 +6,14 @@ const About = () => {
   return (
     <>
     <section>
-      <h1>About US</h1>
-      <p></p>      
+      <h1>about the hotel</h1>
+      <p>Stepping into The Eco & Deluxe feels a little like entering another world. 
+        Elegant, yet effortless. Set apart from the bustle of Manchester, yet immersed 
+        in its culture. Dedicated to fine art, as well as the finer things in life.
+      </p>
+      <p> Every guest receives our world-famous levels of service. Leave the bags to your butler and take in your surroundings. 
+        As you ascend the historic staircase, let The Eco & Deluxe work its magic. Your wish is our command.
+      </p>      
     </section>
     
     <Footer />
